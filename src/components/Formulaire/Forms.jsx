188c1dba6041a4a07cdd9d9070ca51e6228b2916
bollytn <ps3online85@gmail.com>
2024-08-19 12:33:08 +0100

    submit

--- src/components/Formulaire/Forms.jsx
+++ src/components/Formulaire/Forms.jsx
@@ -1,5 +1,25 @@
+import './style.css'
 export default function Forms() {
     return (
-        <div>Form</div>
+        <div className='grp'>
+            <form>
+                <h1>Formulaire</h1>
+                <hr />
+                <label for="nom">Nom:</label>
+                <input type="text" id="nom" name="nom" required />
+                <label for="prenom">Prénom:</label>
+                <input type="text" id="prenom" name="prenom" required />
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+                <label >Vous etes employer ?</label>
+                <input type="checkbox" />
+                <label>Salaire :</label>
+                <select name="" id="">
+                    <option value="">1000Dt</option>
+                    <option value="">2000Dt</option>
+                </select>
+                <input type="submit" />
+            </form>
+        </div>
     )
 }
