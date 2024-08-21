e0a8ec43f0d14f421c1e0d81fd9ff888ca59b252
bollytn <ps3online85@gmail.com>
2024-08-21 15:22:57 +0100

    add file portfolio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Boddy from "./components/Portfolio/Boddy";
-import Foooter from "./components/Portfolio/Foooter";
-import Heeder from "./components/Portfolio/Heeder";
+import Boddy from "./components/Portfolio/Container/Boddy";
+import Foooter from "./components/Portfolio/Footer/Foooter";
+import Heeder from "./components/Portfolio/Header/Heeder";
 
 // import { Container,Footer } from "./components/Portfolio";
 
