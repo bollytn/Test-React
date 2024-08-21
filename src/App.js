13cb1dc38a1af866f83a069879523e75ab1ba569
bollytn <ps3online85@gmail.com>
2024-08-21 18:17:31 +0100

    export all component to index.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import Boddy from "./components/Portfolio/Container/Boddy";
-import Foooter from "./components/Portfolio/Footer/Foooter";
-import Heeder from "./components/Portfolio/Header/Heeder";
 
-// import { Container,Footer } from "./components/Portfolio";
-
+import { Header, Container, Footer } from "./components/Portfolio/index";
 
 const App = () => {
   return (
     <>
-    <Heeder/>
-    <Boddy  />
-    <Foooter/>
+      <Header />
+      <Container />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,3 @@
+export { default as Header } from './Header/Heeder'
+export { default as Container } from './Container/Boddy'
+export { default as Footer } from './Footer/Foooter'
