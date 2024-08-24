6496517e8f73460e8d5c78acc7d58774e954445c
bollytn <ps3online85@gmail.com>
2024-08-24 22:13:31 +0100

    add api tours

--- src/components/15_Project/Tours/Tours.jsx
+++ src/components/15_Project/Tours/Tours.jsx
@@ -2,25 +2,26 @@ import { useEffect, useState } from 'react';
 import './tours.css'
 import Loading from './Loading';
 import Tour from './Tour'
-const url = 'https://www.course-api.com/react-store-products'
+const url = 'https://www.course-api.com/react-tours-project';
 
 export default function Tours() {
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState()
     const [tours, setTours] = useState([])
 
     const fetchTours = async () => {
         setLoading(true);
         try {
-            const response = await fetch(url)
-            const tours = await response.json()
-            setLoading(false)
-            setTours(tours)
-        } catch (error) {
-            setLoading(false)
-            console.log(error);
+            const response = await fetch(url);
+            const tours = await response.json();
+            setLoading(false);
+            setTours(tours);
+            console.log(tours);
             
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
         }
-    }
+    };
 
     useEffect(() => {
         fetchTours();
