15f18d55b12d08aba0643f6dc26e2494e495d2de
bollytn <ps3online85@gmail.com>
2024-08-24 02:19:04 +0100

    page most populaire

--- src/components/Portfolio/sections/MostPopulaire/MostPopulaire.jsx
+++ src/components/Portfolio/sections/MostPopulaire/MostPopulaire.jsx
@@ -1,4 +1,5 @@
 import './MostPopulaire.css'
+import pop_01 from '../../imgs/images/popular-01.jpg'
 export default function MostPopulaire() {
     return (
         <div className='section-wrapper'>
@@ -8,7 +9,62 @@ export default function MostPopulaire() {
             <div className="most-populaire-items">
                 <div className="most-populaire-item">
                     <div className="card-wrapper">
-                        <img src="" alt="" />
+                        <img className='most-populaire-item-img' src={pop_01} alt="pop" />
+                        <div className="most-populaire-item-content">
+                            <h4 className='most-populaire-item-title'>Fortnite
+                                <br />
+                                <span>SandBox</span>
+                            </h4>
+                            <ul>
+                                <li><span>4.8</span></li>
+                                <li><span>2.3M</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="most-populaire-item">
+                    <div className="card-wrapper">
+                        <img className='most-populaire-item-img' src={pop_01} alt="pop" />
+                        <div className="most-populaire-item-content">
+                            <h4 className='most-populaire-item-title'>Fortnite
+                                <br />
+                                <span>SandBox</span>
+                            </h4>
+                            <ul>
+                                <li><span>4.8</span></li>
+                                <li><span>2.3M</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="most-populaire-item">
+                    <div className="card-wrapper">
+                        <img className='most-populaire-item-img' src={pop_01} alt="pop" />
+                        <div className="most-populaire-item-content">
+                            <h4 className='most-populaire-item-title'>Fortnite
+                                <br />
+                                <span>SandBox</span>
+                            </h4>
+                            <ul>
+                                <li><span>4.8</span></li>
+                                <li><span>2.3M</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="most-populaire-item">
+                    <div className="card-wrapper">
+                        <img className='most-populaire-item-img' src={pop_01} alt="pop" />
+                        <div className="most-populaire-item-content">
+                            <h4 className='most-populaire-item-title'>Fortnite
+                                <br />
+                                <span>SandBox</span>
+                            </h4>
+                            <ul>
+                                <li><span>4.8</span></li>
+                                <li><span>2.3M</span></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
