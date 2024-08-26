95b747358ffb4d848b2bf32cbd69e17e7d406a5b
bollytn <ps3online85@gmail.com>
2024-08-26 11:15:10 +0100

    finish accordion

--- src/components/15_Project/Accordion/SingelQuestion.jsx
+++ src/components/15_Project/Accordion/SingelQuestion.jsx
@@ -3,6 +3,10 @@ import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai"
 export default function SingelQuestion({ id, title, info, curOpen, setCurOpen }) {
     const isOpen = curOpen === id;
     const handleToggle = () => {
+        console.log('current open : ' + curOpen);
+        console.log('index : ' + id);
+        console.log('isopne : ' + isOpen);
+
         setCurOpen(isOpen ? null : id)
     }
     return (
