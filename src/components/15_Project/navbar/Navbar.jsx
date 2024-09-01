2303275b094dc7b2922f256d526982bf36caaf88
bollytn <ps3online85@gmail.com>
2024-09-01 18:59:31 +0100

    methode toggle with useref

--- src/components/15_Project/navbar/Navbar.jsx
+++ src/components/15_Project/navbar/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
     const linksRef = useRef(null)
 
     useEffect( () => {
-       const linkH  = linksRef.current.getBoundingClientRect()
+       const linkH  = linksRef.current.getBoundingClientRect().height
         if(showLink) {
             linksContainerRef.current.style.height = `${linkH}px`
         }else {
