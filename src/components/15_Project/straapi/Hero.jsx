766b3c7a01b00a1794d6516b643157b4842574c4
bollytn <ps3online85@gmail.com>
2024-09-03 12:25:59 +0100

    "Implemented Hero, Navbar, and Sidebar components with JSX and React context"

--- src/components/15_Project/straapi/Hero.jsx
+++ src/components/15_Project/straapi/Hero.jsx
@@ -1,12 +1,24 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import phoneImg from './images/phone.svg'
-import {AppContext} from './context'
+import { AppContext } from './context'
 
 const Hero = () => {
-  const data = useContext(AppContext)
-  console.log(data);
-  
-  return <h2>hero component</h2>
+  const { colseSubmenu } = useContext(AppContext)
+
+  return (
+    <section className='hero'>
+      <div className="hero-center">
+        <article className='hero-info'>
+          <h1>payment for the internet</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed vel suscipit autem nam quos recusandae nihil officiis qui deserunt incidunt et necessitatibus quia, earum architecto minus, natus voluptatum voluptas porro.</p>
+          <button className='btn'>Start now</button>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} alt="phone" className='phone-img' />
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
--- src/components/15_Project/straapi/Navbar.jsx
+++ src/components/15_Project/straapi/Navbar.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
+import { AppContext } from './context'
+
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
+  const { openSidebar, openSubmenu, closeSubmenu } = useContext(AppContext)
+  return (
+    <nav className='nav'>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="stripe" className='nav-logo' />
+          <button className='btn toggle-btn' onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'>
+          <li className='link-btn'>
+            <button className='link-btn'>product</button>
+          </li>
+          <li className='link-btn'>
+            <button className='link-btn'>product</button>
+          </li>
+          <li className='link-btn'>
+            <button className='link-btn'>product</button>
+          </li>
+        </ul>
+        <button className='btn signin-btn'>Signin in</button>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
