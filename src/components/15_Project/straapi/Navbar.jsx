44fb621530e5c252592dd8e11dab306753cc76d0
bollytn <ps3online85@gmail.com>
2024-09-04 16:40:05 +0100

    "Added closeSubmenu function to AppContext and updated Navbar component to handle submenu closing on mouseover"

--- src/components/15_Project/straapi/Navbar.jsx
+++ src/components/15_Project/straapi/Navbar.jsx
@@ -5,7 +5,7 @@ import { AppContext } from './context'
 
 
 const Navbar = () => {
-  const { openSidebar, openSubmenu } = useContext(AppContext)
+  const { openSidebar, openSubmenu,closeSubmenu } = useContext(AppContext)
 
   const displayMenu = (e) => {
     const page = e.target.textContent
@@ -15,8 +15,16 @@ const Navbar = () => {
     openSubmenu(page, { center, bottom })
   }
 
+  const handleSubmenu = (e) => {
+   const linkBtn =  e.target.classList.contains('link-btn')
+    if(!linkBtn) {
+    closeSubmenu()
+    }
+    
+  }
+
   return (
-    <nav className='nav'>
+    <nav className='nav' onMouseOver={handleSubmenu}>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="stripe" className='nav-logo' />
