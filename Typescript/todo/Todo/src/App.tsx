3f5e42571ec4c9b21142d8743031dc8f61011741
bollytn <ps3online85@gmail.com>
2024-10-29 02:09:20 +0100

    "Updated App.tsx: added type annotation for todos state, replaced prevTodo with todos in setTodoCompleted function, and replaced dummyData with todos in JSX"

--- Typescript/todo/Todo/src/App.tsx
+++ Typescript/todo/Todo/src/App.tsx
@@ -1,21 +1,21 @@
 import { dummyData } from "./data/todos"
 import TodoItem from "./components/TodoItem"
 import { useState } from "react"
+import { Todo } from "./types/todos"
 
 function App() {
 
-  const [todos, setTodos] = useState(dummyData)
+  const [todos, setTodos] = useState<Todo[]>(dummyData)
 
   function setTodoCompleted(id: number, completed: boolean) {
-    setTodos((prevTodo) =>
-      prevTodo.map((todo) => (todo.id === id ? { ...todo, completed } : todo)))
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, completed } : todo))
   }
   return (
     <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-center text-3xl"> Todo</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
         <div className="space-y-2">
-          {dummyData.map(todo => (
+          {todos.map(todo => (
             <TodoItem
               key={todo.id}
               todo={todo}
