489f15a93f00bc55429acf43284c391b789542c4
bollytn <ps3online85@gmail.com>
2025-02-13 22:49:24 +0100

    protfolio

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 
-// import { Container } from "./components/Portfolio/components/index"
-// import { Header, Footer, Hero, MostPopulaire } from "./components/Portfolio/sections/index";
+import { Container } from "./components/Portfolio/components/index"
+import { Header, Footer, Hero, MostPopulaire } from "./components/Portfolio/sections/index";
 
 
-import Carrousel from './components/Hook/Carrousel'
+// import Carrousel from './components/Hook/Carrousel'
 
 const App = () => {
   return (
     <>
-      <Carrousel />
+      <Header/>
+      <Footer/>
     </>
   )
 }
